5cb48dbd256faa0defe3645f10c2f969baaaa5f7
sebastiankennedy <sebastiankennedy@foxmail.com>
2022-02-06 12:45:58 +0800

    3.4 集成 gin -3

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -19,6 +20,23 @@ func main() {
 		ctx.JSON(http.StatusOK, gin.H{
 			"Hello": "World!",
 		})
+	})
+
+	// 处理 404 请求
+	r.NoRoute(func(ctx *gin.Context) {
+		// 获取标头信息的 Accept 信息
+		acceptString := ctx.Request.Header.Get("Accept")
+
+		if strings.Contains(acceptString, "text/html") {
+			// 如果是 HTML 的话
+			ctx.String(http.StatusNotFound, "页面返回 404")
+		} else {
+			// 默认返回 JSON
+			ctx.JSON(http.StatusNotFound, gin.H{
+				"error_code":    404,
+				"error_message": "路由未定义，请确认 url 和请求方法是否正确",
+			})
+		}
 	})
 
 	// 运行服务，指定端口为 8000
