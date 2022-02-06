f382a4f97dbddbeaa0a14926c78d487133c2fa4a
sebastiankennedy <sebastiankennedy@foxmail.com>
2022-02-06 12:40:46 +0800

    3.4 集成 gin -2

--- main.go
+++ main.go
@@ -7,18 +7,20 @@ import (
 )
 
 func main() {
-	// 初始化 Gin 实例
-	r := gin.Default()
+	// new 一个 Gin Engine 实例
+	r := gin.New()
+
+	// 注册中间件
+	r.Use(gin.Logger(), gin.Recovery())
 
 	// 注册一个路由
 	r.GET("/", func(ctx *gin.Context) {
-
 		// 以 JSON 格式响应
 		ctx.JSON(http.StatusOK, gin.H{
 			"Hello": "World!",
 		})
 	})
 
-	// 运行服务
-	r.Run()
+	// 运行服务，指定端口为 8000
+	r.Run(":8000")
 }
