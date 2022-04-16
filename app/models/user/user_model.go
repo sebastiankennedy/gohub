bf1edb8b4a46b216e2001b597cc69914d0031bda
sebastiankennedy <sebastiankennedy@foxmail.com>
2022-04-16 20:59:05 +0800

    4.3 用户模型 - 2

---
+++ app/models/user/user_model.go
@@ -0,0 +1,14 @@
+package user
+
+import "github.com/sebastiankennedy/gohub/app/models"
+
+type User struct {
+	models.BaseModel
+
+	Name     string `json:"name,omitempty"`
+	Email    string `json:"-"`
+	Phone    string `json:"-"`
+	Password string `json:"-"`
+
+	models.CommonTimestampsField
+}
