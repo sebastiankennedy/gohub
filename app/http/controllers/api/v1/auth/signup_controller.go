bfccbdd8db49ce29830364c2bf54f6395afbcba5
SebastianKennedy <sebastiankennedy@foxmail.com>
2022-04-20 16:21:43 +0800

    4.7 JSON 解析和验证器封装 - 3

--- app/http/controllers/api/v1/auth/signup_controller.go
+++ app/http/controllers/api/v1/auth/signup_controller.go
@@ -7,7 +7,7 @@ import (
 	"github.com/gin-gonic/gin"
 	v1 "github.com/sebastiankennedy/gohub/app/http/controllers/api/v1"
 	"github.com/sebastiankennedy/gohub/app/models/user"
-	requests "github.com/sebastiankennedy/gohub/app/request"
+	requests "github.com/sebastiankennedy/gohub/app/requests"
 )
 
 // 注册控制器
@@ -20,28 +20,8 @@ func (sc *SignupController) IsPhoneExist(c *gin.Context) {
 	// 初始化请求对象
 	request := requests.SignupPhoneExistRequest{}
 
-	// 解析 JSON 请求
-	if err := c.ShouldBindJSON(&request); err != nil {
-		// 解析失败，返回 422 状态码和错误信息
-		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
-			"error": err.Error(),
-		})
-
-		// 打印错误信息
-		fmt.Println(err.Error())
-
-		// 出错了，中断请求
+	if ok := requests.Validate(c, &request, requests.ValidateSignupPhoneExist); !ok {
 		return
-	}
-
-	// 表单验证
-	errs := requests.ValidateSignupPhoneExist(&request, c)
-	// errs 返回长度等于零即通过，大于 0 即有错误发生
-	if len(errs) > 0 {
-		// 验证失败，返回 422 状态码和错误信息
-		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
-			"errors": errs,
-		})
 	}
 
 	// 检查数据库并返回响应
