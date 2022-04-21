fa0fcec720a64f95a7787eeb040f19f60c614353
SebastianKennedy <sebastiankennedy@foxmail.com>
2022-04-21 19:26:03 +0800

    5.2 使用 zap 高性能日志库 - 5

---
+++ bootstrap/logger.go
@@ -0,0 +1,19 @@
+package bootstrap
+
+import (
+	"github.com/sebastiankennedy/gohub/pkg/config"
+	"github.com/sebastiankennedy/gohub/pkg/logger"
+)
+
+func SetupLogger() {
+
+	logger.InitLogger(
+		config.GetString("log.filename"),
+		config.GetInt("log.max_size"),
+		config.GetInt("log.max_backup"),
+		config.GetInt("log.max_age"),
+		config.GetBool("log.compress"),
+		config.GetString("log.type"),
+		config.GetString("log.level"),
+	)
+}
